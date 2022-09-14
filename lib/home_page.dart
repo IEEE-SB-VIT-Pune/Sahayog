0182a7f5b8028bdaf9af4204841cd0e9b9f827e6
aastha9199 <aastha.agarwal21@vit.edu>
2022-09-14 20:46:39 +0530

    bottom nav bar partially added

---
+++ lib/home_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: (Center(child: Text('Home', style: TextStyle(fontSize: 60)))),
+      );
+}
