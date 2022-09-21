51b199ccc6d651922a9473af234e8d2f90a78232
Samarth Thosar <samarth.thosar211@vit.edu>
2022-09-22 01:21:50 +0530

    User auth w/o UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ieee_app_project/screens/home_page.dart';
+import 'package:ieee_app_project/screens/login_page.dart';
 import 'package:ieee_app_project/widgets/bottom_nav_bar.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,3 +21,23 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context)  =>Scaffold(
+    body:StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData){
+          return HomePage();
+        }
+        else{
+          return LoginPage();
+        } 
+      }
+    ),
+  );
+    
+  }
+   
