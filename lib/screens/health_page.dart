ff331d12bbad0854a63843b091bbe788e6068e51
Akshata <akshata.gile21@vit.edu>
2022-10-14 19:27:08 +0530

    minor changes

--- lib/screens/health_page.dart
+++ lib/screens/health_page.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
