7ce4b2a68fd7a121b200681d7b4f0ac1a5e5d85e
aastha9199 <aastha.agarwal21@vit.edu>
2022-10-18 17:15:49 +0530

    Minor changes done/Settings,Profile Page

--- lib/screens/Settings/add_contact.dart
+++ lib/screens/Settings/add_contact.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ieee_app_project/models/user_model.dart';
+import 'package:ieee_app_project/screens/Settings/Contact_page.dart';
 
 class Contact_Add extends StatefulWidget {
   const Contact_Add({super.key});
@@ -32,32 +34,47 @@ class _Contact_AddState extends State<Contact_Add> {
     h = MediaQuery.of(context).size.height;
     w = MediaQuery.of(context).size.width;
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Add contact',
-            style: GoogleFonts.montserrat(
-                color: Colors.black,
-                fontSize: w / 15,
-                fontWeight: FontWeight.w500),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          elevation: 0,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Add contact',
+          style: GoogleFonts.montserrat(
+              fontSize: w / 17,
+              color: Colors.black,
+              fontWeight: FontWeight.w700),
         ),
-        body: Stack(
-          children: [
-            Container(
-              padding: EdgeInsets.all(w / 20),
-            ),
-            SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: FaIcon(FontAwesomeIcons.angleLeft),
+          color: Colors.black,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(padding: EdgeInsets.all(h / 40)),
+              Row(
                 children: [
-                  Padding(padding: EdgeInsets.all(h / 40)),
-                  SizedBox(
-                    height: h / 20,
-                    width: h / 3,
+                  SizedBox(width: 30 * w / 360),
+                  Icon(
+                    Icons.person,
+                    color: Color(0xff0C5DAD),
+                    size: 28.5 * w / 360,
+                  ),
+                  SizedBox(width: 15 * w / 360),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0XffFFFFFF),
+                    ),
+                    height: 53 * h / 640,
+                    width: 250 * w / 360,
                     child: TextFormField(
                       // Field to enter name
                       controller: nameEditingController,
@@ -74,10 +91,24 @@ class _Contact_AddState extends State<Contact_Add> {
                       ),
                     ),
                   ),
-                  SizedBox(height: h / 40),
-                  SizedBox(
-                    height: h / 20,
-                    width: h / 3,
+                ],
+              ),
+              SizedBox(height: h / 40),
+              Row(
+                children: [
+                  SizedBox(width: 30 * w / 360),
+                  Icon(
+                    Icons.phone,
+                    color: Color(0xff0C5DAD),
+                    size: 28.5 * w / 360,
+                  ),
+                  SizedBox(width: 15 * w / 360),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0XffFFFFFF),
+                    ),
+                    height: 53 * h / 640,
+                    width: 250 * w / 360,
                     child: TextFormField(
                       //Field to enter phone number
                       controller: phoneEditingController,
@@ -94,76 +125,88 @@ class _Contact_AddState extends State<Contact_Add> {
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: h / 40,
-                  ),
-                  SizedBox(
-                      height: h / 20,
-                      width: w / 2.5,
-                      child: DropdownButtonFormField(
-                          //Drop to select relation
-                          value: selected,
-                          onChanged: (item) => setState(() => selected = item),
-                          items: relations
-                              .map((item) => DropdownMenuItem(
-                                  value: item,
-                                  child: Text(item,
-                                      style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.grey.shade700))))
-                              .toList(),
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.all(10),
-                            fillColor: Colors.grey.shade100,
-                            filled: true,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                          ))),
-                  SizedBox(
-                    height: h / 40,
-                  ),
-                  SizedBox(
-                      height: h / 20,
-                      width: h / 0.5,
-                      child: Center(
-                          child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.deepPurple.shade500,
-                            minimumSize: const Size.fromWidth(200),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20))),
-                        onPressed: (() async {
-                          FirebaseFirestore firebaseFirestore =
-                              FirebaseFirestore.instance;
-                          User? user = _auth.currentUser;
-                          for (int i = 0; i >= 10; i++) {
-                            contactIndex++;
-                          }
-                          UserModel usm = UserModel();
-
-                          usm.emergencyName = nameEditingController.text;
-                          usm.phone = phoneEditingController.text;
-                          usm.contactIndex = contactIndex;
-
-                          await firebaseFirestore
-                              .collection("users")
-                              .doc(FirebaseAuth.instance.currentUser!.uid)
-                              .collection("Contacts")
-                              // .orderBy('contactIndex', descending: true)
-                              .add(usm.contactMap())
-                              .then((value) {
-                            Navigator.pop(context);
-                            Fluttertoast.showToast(
-                                msg: "Contact added successfully");
-                          }).catchError((error) =>
-                                  print("Failed to add a new contact $error"));
-                        }),
-                        child: Text("Save"),
-                      ))),
                 ],
               ),
-            )
-          ],
-        ));
+              SizedBox(
+                height: h / 40,
+              ),
+              SizedBox(
+                  height: h / 20,
+                  width: w / 2.5,
+                  child: DropdownButtonFormField(
+                      //Drop to select relation
+                      value: selected,
+                      onChanged: (item) => setState(() => selected = item),
+                      items: relations
+                          .map((item) => DropdownMenuItem(
+                              value: item,
+                              child: Text(item,
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.grey.shade700))))
+                          .toList(),
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10),
+                        fillColor: Colors.grey.shade100,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                      ))),
+              SizedBox(
+                height: h / 30,
+              ),
+              InkWell(
+                child: Container(
+                    height: 31 * h / 640,
+                    width: 81 * w / 360,
+                    decoration: BoxDecoration(
+                        color: Color(0Xff7FB77E),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                      child: Text(
+                        "Save",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 20 * w / 360,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0XffFFFFFF)),
+                      ),
+                    )),
+                onTap: (() async {
+                  FirebaseFirestore firebaseFirestore =
+                      FirebaseFirestore.instance;
+                  User? user = _auth.currentUser;
+                  for (int i = 0; i >= 10; i++) {
+                    contactIndex++;
+                  }
+                  UserModel usm = UserModel();
+
+                  usm.emergencyName = nameEditingController.text;
+                  usm.phone = phoneEditingController.text;
+                  usm.contactIndex = contactIndex;
+
+                  await firebaseFirestore
+                      .collection("users")
+                      .doc(FirebaseAuth.instance.currentUser!.uid)
+                      .collection("Contacts")
+                      .add(usm.contactMap())
+                      .then((value) {
+                    FirebaseFirestore.instance
+                        .collection("users")
+                        .doc(FirebaseAuth.instance.currentUser!.uid)
+                        .collection("Contacts")
+                        .get()
+                        .then((value) => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    ContactsPage(ContactRef: value))));
+                    Fluttertoast.showToast(msg: "Contact added successfully");
+                  }).catchError((error) =>
+                          print("Failed to add a new contact $error"));
+                }),
+              )
+            ]),
+      ),
+    );
   }
 }
